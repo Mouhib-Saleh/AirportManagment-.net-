9dbd706479ae37ad8692991775a9a51016e11e3e
mouhib saleh <mouhib.saleh20@gmail.com>
2023-02-26 11:43:20 +0100

    db added

--- AM.ApplicationCore/Domain/Passanger.cs
+++ AM.ApplicationCore/Domain/Passanger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace AM.ApplicationCore.Domain
     public class Passanger
     {
        public DateTime BirthDate { get; set; }
+        [Key]
        public int PassportNumber { get; set; }
         public string EmailAddress { get; set; }
         public string FirstName { get; set; }
---
+++ AM.Infrastructure/AmContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AM.ApplicationCore.Domain;
+namespace AM.Infrastructure
+{
+    public class AmContext: DbContext
+    {
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Traveller> Travellers { get; set; }
+        public DbSet<Passanger> Passengers { get; set; }
+        public DbSet<Plane> Planes { get; set; }
+        public DbSet<Staff> staff { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=(localdb)\mssqllocaldb;initial catalog = MouhibSaleh;integrated security = true");
+        }
+
+    }
+}
